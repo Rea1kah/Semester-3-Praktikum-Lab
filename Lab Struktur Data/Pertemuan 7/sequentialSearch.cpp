91fbd1c1bfb0345285cc28ba96421ba316b6e75b
Rea1kah <arielafriza88@gmail.com>
2025-11-24 11:25:52 +0700

    Add files via upload

---
+++ Lab Struktur Data/Pertemuan 7/sequentialSearch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, key, posisi = -1;
+
+    cout << "Masukkan jumlah data: ";
+    cin >> n;
+    vector<int> data(n);
+
+    cout << "Masukkan elemen data: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << i << ": ";
+        cin >> data[i];
+    }
+
+    cout << "\nMasukkan nilai yang ingin dicari: ";
+    cin >> key;
+
+    // TODO : Proses pencarian sequential/linear
+    for(int i = 0; i < n; i++)
+    {
+        if(data[i] == key)
+        {
+            posisi = i; // simpan posisi jika ditemukan
+            break; // hentikan pencarian setelah menemukan
+        }
+    }
+
+    // Output hasil
+    if (posisi != -1)
+        cout << "\nData ditemukan di indeks ke-" << posisi << endl;
+    else
+        cout << "\nData tidak ditemukan." << endl;
+
+    return 0;
+}
